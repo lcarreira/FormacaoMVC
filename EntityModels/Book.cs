0e69316fd53a1bc4693fd63cf4f73520dcb70883
ndmarques <nelson.marques@pessoaseprocessos.com>
2015-02-19 15:40:59 +0000

    test1

--- EntityModels/Book.cs
+++ EntityModels/Book.cs
@@ -11,7 +11,7 @@ namespace EntityModels
     public class Book
     {
         [DataMember]
-        public int Id { get; set; }
+        public int Id { get; set;               }
 
         [DataMember]
         public string Title { get; set; }
