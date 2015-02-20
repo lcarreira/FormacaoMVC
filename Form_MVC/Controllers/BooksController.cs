2cb685de47cc45d7db5c0ee23a0198ef1352cf4a
glaranjeira <glaranjeira@pessoaseprocessos.com>
2015-02-20 09:49:15 +0000

    listcommit

---
+++ Form_MVC/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form_MVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            FromWCF.BibliotecaServiceClient BibliotecaService = new FromWCF.BibliotecaServiceClient();
+            List<EntityModels.Book> BookList = BibliotecaService.GetBooks();
+
+            ViewBag.Books = BookList;
+            return View();
+        }
+    }
+}
